6a698440d5bee4ed7691a1487b8e1fb0a5e80cfc
xiejf <xiejingfan1024@gmail.com>
2023-08-16 12:31:10 +0800

    null safety

--- example/lib/util/extension.ivybaby.api.dart
+++ example/lib/util/extension.ivybaby.api.dart
@@ -1,5 +1,6 @@
 import 'package:ivybaby_api/annotation/api_extension.dart';
 
+/// import 'package:ivybaby_api/api/base/api_base.dart';
 /// 生成ivy baby域名相关的api
 @ApiExtension(className: 'IvyBabyApiImpls', key: '\${ApiConfig.ivyBase}')
 class IvyBabyApis {
--- example/lib/util/extension.mall.api.dart
+++ example/lib/util/extension.mall.api.dart
@@ -1,5 +1,6 @@
 import 'package:ivybaby_api/annotation/api_extension.dart';
 
+/// import 'package:ivybaby_api/api/base/api_base.dart';
 /// 生成mall域名相关的api
 @ApiExtension(className: 'MallApiImpls', key: '\${ApiConfig.mallBase}')
 class MallApis {
